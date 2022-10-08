3043c9e9d9e1f381eed668086eee3e4b41fa0ec3
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-08 17:16:50 +0530

    bitwise operator

---
+++ bitwise operator/bit1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a = 2;
+    int b = 4;
+    cout <<"(a & b) = "<<(a & b) << endl;
+    cout <<"(a ^ b) = "<<(a ^ b) << endl;
+    cout <<"(a | b) = "<<(a | b) << endl;
+    cout <<"~b = "<<~b << endl;
+    cout <<"~a = "<<~a << endl;
+
+    return 0;
+}
