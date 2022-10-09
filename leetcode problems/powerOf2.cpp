e59a244e5f630dd981d726ce70c57636dbc956e9
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-09 22:23:52 +0530

    code generalize kr diya

--- leetcode problems/powerOf2.cpp
+++ leetcode problems/powerOf2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-bool isPowerOfTwo(int n){
+bool isPowerOfTwo(int n,int k){
     int ans = 1;
     for (int i = 0; i <= 30; i++)
     {
@@ -9,17 +9,22 @@ bool isPowerOfTwo(int n){
             return true;  // we can use pow func but it is not optimize solution
          }
          if(ans<INT_MAX/3)
-         ans = ans*3;
+         ans = ans*k;
     }
     return false;
     
 }
 int main()
 {
-        int t;
+        int t,k;
+        cout<<"enter number is Power Of k  : ";
         cin>>t; 
+        cout<<" enter the value of k ";
+        cin>>k; 
 
-  cout<<isPowerOfTwo(t);
+
+  cout<<isPowerOfTwo(t,k);
+  // wa mja aa gya 😍
 
     return 0;
 }
