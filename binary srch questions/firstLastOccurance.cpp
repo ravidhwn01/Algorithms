13f176033062e2a76bf97f7ad1457a00a162aa97
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-16 15:33:35 +0530

    peak element in  array

--- binary srch questions/firstLastOccurance.cpp
+++ binary srch questions/firstLastOccurance.cpp
@@ -36,10 +36,10 @@ int lastOcc(int arr[],int n,int key){
             ans  = mid;
             s = mid+1;
         }
-        else if(arr[mid]<key){   // right me jao
+        else if(arr[mid]<key){   
             s = mid+1;
         }
-        else if(arr[mid]>key){ // left me jao
+        else if(arr[mid]>key){ 
                 e=mid-1;
         }
       mid = s+(e-s)/2;
@@ -49,6 +49,7 @@ int lastOcc(int arr[],int n,int key){
 }
 int main()
 {
+    //  complexity  is O(log(N))
     int arr[]= {1,2,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<"first occurance of  at index : "<<firstOcc(arr,n,4)<<endl;
--- binary srch questions/peakElement.cpp
+++ binary srch questions/peakElement.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
+// kisi mountain array me peak element 
 int peakelementInArray(int* arr,int high){
     int s = 0;
     int e = high-1;
@@ -18,6 +19,7 @@ int peakelementInArray(int* arr,int high){
 }
 int main()
 {
+    //  complexity  is O(log(N))
     int arr[]= {1,2,4,4,4,6,7,8,9,5,4,3,2,0};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<"the peak  elements  index is : "<<peakelementInArray(arr,n)<<endl;
