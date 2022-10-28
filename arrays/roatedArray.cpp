8a2ff6645c5a2617e55ec9514d1aebac5ebe634f
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-28 22:35:40 +0530

    roted a array

---
+++ arrays/roatedArray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int printarray(int *a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[] = {2,3,4,6,43,7,8,9};
+
+    return 0;
+}
