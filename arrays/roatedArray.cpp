e8ee6d8a2506e2f00e99bdce3229801f496e917d
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-29 22:10:07 +0530

    rotate the  array

--- arrays/roatedArray.cpp
+++ arrays/roatedArray.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int printarray(int *a, int n)
+void rotate(vector<int> &nums, int k)
 {
+    int n = nums.size();
+    vector<int> temp(n);
     for (int i = 0; i < n; i++)
     {
-        cout << a[i] << " ";
+        temp[(i + k) % n] = nums[i];
     }
-    cout << endl;
+    // copy temp into num vector
+    nums = temp;
 }
 int main()
 {
-    int arr[] = {2,3,4,6,43,7,8,9};
-
+    vector<int> arr = {2, 3, 4, 6, 43, 7, 8, 9};
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    rotate(arr, 3);
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
